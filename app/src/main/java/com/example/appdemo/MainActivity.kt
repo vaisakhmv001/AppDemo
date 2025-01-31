7e127083c51e1e8944031b1b7cb27cf656a856d1
vaisakh mv <vaisakh.mv001@gmail.com>
2025-01-31 10:51:49 +0530

    User Interface

---
+++ app/src/main/java/com/example/appdemo/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.appdemo
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.appdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+}
